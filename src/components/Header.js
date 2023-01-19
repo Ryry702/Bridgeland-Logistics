e893b0ae6a1e00dcd6b9329266ecb6d834bc7639
Ryan Hathaway <rryanhathaway@gmail.com>
2023-01-19 15:26:56 -0800

    added Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import '../styles/Header.css';
+
+const Header = (props) => {
+    return (
+        <header>
+            <div className='HeaderTitle'>
+                <h1>image logo place holder</h1>
+                <h1>Bridgeland Logistics</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
